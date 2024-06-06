c5d643dc82eec48c40c0ced1b15c00c9f66dc7a3
lijoantony7 <lijoa1997@gmail.com>
2024-06-06 23:13:13 +0530

    redirectTo and pathMatch (docu added)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
+import { AboutUsComponent } from './about-us/components/about-us/about-us.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent},
+  { path: '', redirectTo: 'about-us', pathMatch: 'full'},
+  { path: 'about-us', component: AboutUsComponent},
   { path: '**', component: NotFoundComponent},
 ];
 
